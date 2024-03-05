da662eee4e094510bc4bc6d3979e08df2a9fef17
aaruanbu <aaruanbu001@gmail.com>
2024-03-05 13:33:54 +0530

    swe

--- src/Components/Bottom.jsx
+++ src/Components/Bottom.jsx
@@ -44,7 +44,6 @@ export const Bottom = () => {
             <MdLiveHelp />
           </Box>
           <Box>
-            {/* <MdAccountCircle /> */}
             <Drops />
           </Box>
         </Box>
